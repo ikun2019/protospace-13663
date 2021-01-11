e45548f120dc23cfa1d68c8f885a509a1a7cfd77
ikun <isao.miyagawa.2019@gmail.com>
2021-01-11 23:25:38 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,10 +3,22 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    prototype = Prototype.new(create_params)
+    if prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    
   end
   
+  private
+  def create_params
+    params.permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
   
 end
