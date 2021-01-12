6af25488e62e3f163868849dab418085314342cf
ikun <isao.miyagawa.2019@gmail.com>
2021-01-12 20:24:10 +0900

    コントローラーとアクション、ルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:create]
   end
+  resources :users, only: [:show]
 end
